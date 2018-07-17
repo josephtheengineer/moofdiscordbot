62276ab74924e4d97532294fd995c1a03c558100
JosephTheEngineer <jiuzzolino@outlook.com>
2018-07-18 06:39:08 +1000

    Added +help

--- index.js
+++ index.js
@@ -5,41 +5,54 @@ const Discord = require('discord.js')
 // Create an instance of Discord that we will use to control the bot
 const bot = new Discord.Client();
 
-const config = require("./config.json");
+const config = require('./config.json');
+
+const prefix = '+'
 
 // Gets called when our bot is successfully logged in and connected
-bot.on('ready', () => {
-    console.log('Infinity AI - Discord Module - Active');
+bot.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `bot.user` is what the
+  // docs refer to as the "ClientUser".
+  bot.user.setActivity(`+help`);
+});
+
+bot.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  bot.user.setActivity(`+help`);
+});
+
+bot.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  bot.user.setActivity(`+help`);
 });
 
 // Event to listen to messages sent to the server where the bot is located
-bot.on('message', message => {
+bot.on("message", async message => {
     // So the bot doesn't reply to iteself
-    if (message.author.bot) return;
+	if(message.author.bot) return;
+  
+	// Also good practice to ignore any message that does not start with our prefix, 
+	if(message.content.indexOf(prefix) !== 0) return;
     
-    var text = message.content;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
     
-    if (text == 'ping'){
-      message.reply('pong!');
-    }
-    
-    // Check if the message starts with the `!` trigger
-/*    if (message.content.indexOf('!') === 0) {
-        // Get the user's message excluding the `!`
-        var text = message.content.substring(1);
-        
-        // Reverse the message
-        var reversed = '';
-        var i = text.length;
-        
-        while (i > 0) {
-            reversed += text.substring(i - 1, i);
-            i--;
-        }
-        
-        // Reply to the user's message
-        message.reply(reversed);
-    }*/
+	if(command === "ping") {
+		// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+		// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+		const m = await message.channel.send("Ping?");
+		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
+	}
+	
+	if(command === "help") {
+		message.channel.send(`**Commands**
+		+help
+		+ping`);
+	}
 });
 
 
