53f6b82555d6ea8235b50102db8bcc51e807e668
JosephTheEngineer <jiuzzolino@outlook.com>
2018-10-19 21:44:49 +1000

    Worked on help menu + bot doesn't require prefix in #bot

---
+++ DiscordBot/help.md
@@ -0,0 +1,11 @@
+```Moof Commands```
+  `help`
+  `ping`
+```Tatsumaki Commands```
+  **Core -** `help` `invite` `leveledroles` `ping` `selfroles` `support`
+  **Social -** `background` `badges` `dailies` `played` `profile` `rank` `reputation` `setinfo` `settitle` `top`
+  **Fun -** `8ball` `cah` `cat` `catfacts` `choose` `coin` `cookie` `dice` `dog` `dogfacts` `fishy` `fortune` `numberfacts` `psychopass` `ratewaifu` `reverse` `rps` `slots tags` `tatsugotchi` `beautiful` `hackerman`
+  **Economy -** `credits` `points`
+  **Utilities -** `color` `converter` `crypto` `google` `image` `lotto` `math` `remindme` `shorten` `stocks` `strawpoll` `todo` `urban` `vote` `weather` `wiki` `youtube`
+  **Information -** `apikey` `avatar` `botlist` `changelog` `channel` `info` `role` `server` `shared` `usage`
+  **Anime -** `anime` `manga` `osu` `sic`
--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -1,107 +1,141 @@
 
-// Import the discord.js module
 const Discord = require('discord.js')
+const fs = require('fs')
+const path = require('path')
 
-// Create an instance of Discord that we will use to control the bot
-const bot = new Discord.Client();
+const bot = new Discord.Client()
 
-const config = require('./config.json');
 
+const config = require('./config.json')
 const prefix = '+'
 
-// Gets called when our bot is successfully logged in and connected
-bot.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
-  // Example of changing the bot's playing game to something useful. `bot.user` is what the
-  // docs refer to as the "ClientUser".
-  bot.user.setActivity(`+help`);
-});
+//==============================================================================
+// file(command) | Gets a commands output from its text file
+//==============================================================================
+function file(command){
+  return fs.readFileSync(command + '.md','utf8')
+}
 
-bot.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  bot.user.setActivity(`+help`);
-});
+//==============================================================================
+// ready | Gets called when our bot is successfully logged in and connected.
+//==============================================================================
+bot.on('ready', () => {
+  const guild = bot.guilds.array()[0];
+  const channel = guild.channels.find('name', 'bot')
 
-bot.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  bot.user.setActivity(`+help`);
-});
+  console.log( `Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
+  //channel.send(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
 
-// Event to listen to messages sent to the server where the bot is located
-bot.on("message", async message => {
+  bot.user.setActivity(`+help`)
+})
+
+//==============================================================================
+// guildCreate | This event triggers when the bot joins a guild.
+//==============================================================================
+bot.on('guildCreate', guild => {
+  bot.user.setActivity(`+help`)
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`)
+})
+
+//==============================================================================
+// guildDelete | This event triggers when the bot is removed from a guild.
+//==============================================================================
+bot.on('guildDelete', guild => {
+  bot.user.setActivity(`+help`)
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`)
+})
+
+//==============================================================================
+// message | Event to listen to messages sent to the server where the bot is located
+//==============================================================================
+bot.on('message', async message => {
   // So the bot doesn't reply to iteself
-	if(message.author.bot) return;
+	if(message.author.bot) return
+  const botChannel = message.guild.channels.find('name', 'bot')
 
-	// Also good practice to ignore any message that does not start with our prefix,
-	if(message.content.indexOf(prefix) !== 0) return;
+  var args = ''
+  var command = ''
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
+  if(message.channel != botChannel){
+    // Ignore any message that does not start with our prefix,
+    if(message.content.indexOf(prefix) !== 0) return
+    args = message.content.slice(prefix.length).trim().split(/ +/g)
+  	command = args.shift().toLowerCase()
+  } else {
+    args = message.content.trim().split(/ +/g)
+    command = args.shift().toLowerCase()
+  }
 
   console.log(command)
+  console.log(args)
 
-	if(command === "ping") {
-		// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-		// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-		const m = await message.channel.send("Ping?");
-		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
-	}
+  switch(command) {
+    case 'ping':
+                    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+                		const m = await message.channel.send('Ping?')
+                		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`)
+    break;
+    case 'help':
+                    for (var i = 0; i < args.length; i++) {
+                      if (args[i] == 'adv') {
+                        message.channel.send(file('help'))
+                      }
+                    }
+    break;
+    default:
+                    if(message.channel != botChannel){
+                      message.channel.send('Command not found')
+                    }
+}
+})
 
-	if(command === "help") {
-		message.channel.send(`**Commands**
-		+help
-		+ping`);
-	}
-});
-
-// Create an event listener for new guild members
+//==============================================================================
+// guildMemberAdd | Create an event listener for new guild members
+//==============================================================================
 bot.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  //const channel = member.guild.channels.find('name', 'member-log');
+  const channel = member.guild.channels.find('name', 'welcome')
   // Do nothing if the channel wasn't found on this server
-  //if (!channel) return;
-  // Send the message, mentioning the member
+  if (!channel) return
 
   console.log(`Welcome to the server, ${member.displayName}`)
-  if(member.displayName.indexOf("discord.gg") > -1 || member.displayName.indexOf("add me") > -1 || member.displayName.indexOf("twitch.tv")) {
+
+  if(member.displayName.indexOf('discord.gg') > -1 || member.displayName.indexOf('add me') > -1 || member.displayName.indexOf('twitch.tv')) {
     member.guild.ban(member)
     console.log(`SPAM DECTECTED: ${member}`)
-    const adminChannel = member.guild.channels.find('name', 'admin-chat');
-    console.log(adminChannel);
-    adminChannel.send(`SPAM DECTECTED: ${member}`);
+    const adminChannel = member.guild.channels.find('name', 'admin-chat')
+    console.log(adminChannel)
+    adminChannel.send(`SPAM DECTECTED: ${member}`)
   } else {
-    const welcomeChannel = member.guild.channels.find('name', 'welcome');
-    welcomeChannel.send(`Welcome ${member} to the discord server for Eden: Universe Builder! :D
+    channel.send(`Welcome ${member} to the discord server for Eden: Universe Builder! :D
 
-Please wait for a <@&468233990642335745> or a <@&468233437115842560> to add you to the trusted members group!`);
+Please wait for a <@&468233990642335745> or a <@&468233437115842560> to add you to the trusted members group!`)
   }
-  //channel.send(`Welcome to the server, ${member}`);
-});
+  //channel.send(`Welcome to the server, ${member}`)
+})
 
-// Create an event listener for new guild members
+//==============================================================================
+// guildMemberRemove | Create an event listener for new guild members
+//==============================================================================
 bot.on('guildMemberRemove', member => {
   // Send the message to a designated channel on a server:
-  //const channel = member.guild.channels.find('name', 'member-log');
+  //const channel = member.guild.channels.find('name', 'member-log')
   // Do nothing if the channel wasn't found on this server
-  //if (!channel) return;
+  //if (!channel) return
   // Send the message, mentioning the member
 
   console.log(`We're sorry to see you leaving, ${member.displayName}`)
-  if(member.displayName.indexOf("discord.gg") > -1 || member.displayName.indexOf("add me") > -1) {
+  if(member.displayName.indexOf('discord.gg') > -1 || member.displayName.indexOf('add me') > -1) {
     //member.guild.ban(member)
     //console.log(`SPAM DECTECTED: ${member}`)
-    //const adminChannel = member.guild.channels.find('name', 'admin-chat');
-    //console.log(adminChannel);
-    //adminChannel.send(`SPAM DECTECTED: ${member}`);
+    //const adminChannel = member.guild.channels.find('name', 'admin-chat')
+    //console.log(adminChannel)
+    //adminChannel.send(`SPAM DECTECTED: ${member}`)
   } else {
-    const welcomeChannel = member.guild.channels.find('name', 'welcome');
-    welcomeChannel.send(`We're sorry to see you leaving, ${member}`);
+    const welcomeChannel = member.guild.channels.find('name', 'welcome')
+    welcomeChannel.send(`We're sorry to see you leaving, ${member}`)
   }
-  //channel.send(`Welcome to the server, ${member}`);
-});
+  //channel.send(`Welcome to the server, ${member}`)
+})
 
 
-bot.login(config.token);
+bot.login(config.token)
