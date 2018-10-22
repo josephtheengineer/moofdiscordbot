ead2350f130cfeb36ac504c44c123a64eaaf7f3d
JosephTheEngineer <jiuzzolino@outlook.com>
2018-10-23 06:39:03 +1000

    added discord.me to the blocklist

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bot = new Discord.Client()
 
 const config = require('./config.json')
 const prefix = 'm!'
+var msgSent = false
 
 //==============================================================================
 // file(command) | Gets a commands output from its text file
@@ -22,11 +23,11 @@ async function activity(){
           name: `bot started!`,
           type: "PLAYING"
       }
-  });
+  })
 
   while(true){
     await sleep(10000)
-    var x = Math.floor(Math.random() * Math.floor(9));
+    var x = Math.floor(Math.random() * Math.floor(9))
     switch (x){
       case 0:
                 bot.user.setPresence({
@@ -34,70 +35,70 @@ async function activity(){
                         name: `Eden: Universe Builder`,
                         type: "PLAYING"
                     }
-                });
+                })
       case 1:
                 bot.user.setPresence({
                     game: {
                         name: `m!help`,
                         type: "LISTENING"
                     }
-                });
+                })
       case 2:
                 bot.user.setPresence({
                     game: {
                         name: `${bot.users.size} builders!`,
                         type: "WATCHING"
                     }
-                });
+                })
       case 3:
                 bot.user.setPresence({
                     game: {
                         name: `with Stumpy`,
                         type: "PLAYING"
                     }
-                });
+                })
       case 4:
                 bot.user.setPresence({
                     game: {
                         name: `edengame.net`,
                         type: "STREAMING"
                     }
-                });
+                })
       case 5:
                 bot.user.setPresence({
                     game: {
                         name: `r/edengame`,
                         type: "STREAMING"
                     }
-                });
+                })
       case 6:
                 bot.user.setPresence({
                     game: {
                         name: `gitlab.com/edenproject`,
                         type: "STREAMING"
                     }
-                });
+                })
       case 7:
                 bot.user.setPresence({
                     game: {
                         name: `discord.me/EdenUniverseBuilder`,
                         type: "WATCHING"
                     }
-                });
+                })
       case 8:
                 bot.user.setPresence({
                     game: {
                         name: `#bot`,
                         type: "WATCHING"
                     }
-                });
+                })
       default:
                 bot.user.setPresence({
                     game: {
                         name: `Eden: Universe Builder`,
                         type: "PLAYING"
                     }
-                });
+                })
     }
   }
 }
@@ -112,13 +113,14 @@ function sleep(ms){
 // ready | Gets called when our bot is successfully logged in and connected.
 //==============================================================================
 bot.on('ready', () => {
-  const guild = bot.guilds.array()[0];
+  const guild = bot.guilds.array()[0]
   const channel = guild.channels.find('name', 'bot')
 
   console.log( `Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
   //channel.send(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
 
-  activity();
+  activity()
+  //displayPrompt()
 })
 
 //==============================================================================
@@ -135,13 +137,39 @@ bot.on('guildDelete', guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`)
 })
 
+async function displayPrompt(command){
+  const guild = bot.guilds.array()[0]
+  const channel = guild.channels.find('name', 'bot')
+
+  while(true){
+    await sleep(10000)
+    if (msgSent){
+      channel.send(file('help'))
+      .then(msg => {
+        msg.delete(10000)
+      })
+      .catch()
+      msgSent = false
+    }
+  }
+}
+
 //==============================================================================
 // message | Event to listen to messages sent to the server where the bot is located
 //==============================================================================
 bot.on('message', async message => {
+
+  const log = fs.readFileSync('log.txt','utf8') + message.author.username + ' | ' + message.content + '\n'
+  fs.writeFile("log.txt", log, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+  })
+
   // So the bot doesn't reply to iteself
 	if(message.author.bot) return
   const botChannel = message.guild.channels.find('name', 'bot')
+  msgSent = true
 
   var args = ''
   var command = ''
@@ -168,7 +196,7 @@ bot.on('message', async message => {
                     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
                 		const m = await message.channel.send('Ping?')
                 		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`)
-    break;
+    break
     case 'help':
                     if (args.length <= 0){
                       message.channel.send(file('help'))
@@ -185,7 +213,7 @@ bot.on('message', async message => {
                         }
                       }
                     }
-    break;
+    break
     default:
                     if(message.channel != botChannel){
                       message.channel.send('Command not found')
@@ -204,7 +232,7 @@ bot.on('guildMemberAdd', member => {
 
   console.log(`Welcome to the server, ${member.displayName}`)
 
-  if (member.displayName.indexOf('discord.gg') > -1 || member.displayName.indexOf('add me') > -1 || member.displayName.indexOf('twitch.tv') > -1) {
+  if (member.displayName.indexOf('discord.gg') > -1 || member.displayName.indexOf('add me') > -1 || member.displayName.indexOf('twitch.tv') > -1 || member.displayName.indexOf('discord.me') > -1) {
     member.guild.ban(member)
     console.log(`SPAM DETECTED: ${member}`)
     console.log(adminChannel)
@@ -215,7 +243,7 @@ bot.on('guildMemberAdd', member => {
             name: `the new guy`,
             type: "WATCHING"
         }
-    });
+    })
     channel.send(`Welcome ${member} to the discord server for Eden: Universe Builder! :D
 
 Please wait for a <@&468233990642335745> or a <@&468233437115842560> to add you to the trusted members group!`)
@@ -232,13 +260,13 @@ bot.on('guildMemberRemove', member => {
 
   console.log(`We're sorry to see you leaving, ${member.displayName}`)
 
-  if (!(member.displayName.indexOf('discord.gg') > -1 || member.displayName.indexOf('add me') > -1 || member.displayName.indexOf('twitch.tv') > -1)) {
+  if (!(member.displayName.indexOf('discord.gg') > -1 || member.displayName.indexOf('add me') > -1 || member.displayName.indexOf('twitch.tv') > -1 || member.displayName.indexOf('discord.me') > -1)) {
     bot.user.setPresence({
         game: {
             name: `the player that left`,
             type: "WATCHING"
         }
-    });
+    })
     channel.send(`We're sorry to see you leaving, ${member} :cancel:`)
   }
 })
